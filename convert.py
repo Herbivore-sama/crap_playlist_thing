6f453ba36246d076f1255b6e89cb1b5952b018af
cdaher <c_daher2@massbay.edu>
2022-07-10 03:22:59 -0400

    It's a start, I guess

---
+++ convert.py
@@ -0,0 +1,31 @@
+import os,shutil
+
+def convert(basePath, newPath, file, destDirectory):
+    with open(file, "r") as plfile, open(os.path.join(destDirectory, os.path.basename(file)), "w") as destfile:
+        destfile.write("#EXTM3U\n")
+        musicfiles = [i for i in plfile if i != "#EXTM3U\n"]
+        for i in musicfiles:
+            destfile.write(i.replace(basePath, newPath).replace("\\", "/"))
+        plfile.close()
+        destfile.close()
+
+def copy(file, destDirectory):
+    with open(file, "r") as plfile:
+        musicfiles = [i for i in plfile if i != "#EXTM3U\n"]
+        for i in musicfiles:
+            try:
+                shutil.copyfile(i, os.path.join(destDirectory, os.path.basename(i)))
+            except:
+                print("Playlist file contains invalid path, and/or destination is invalid")
+
+class fileInfo:
+    def __init__(self):
+        self.basePath = None
+        self.newPath = None
+        self.playlistPath = None
+        
+    def setplaylistPath(self, x):
+        self.playlistPath = x
+    
+    def getplaylistPath(self):
+        return self.playlistPath
---
+++ gui.py
@@ -0,0 +1,116 @@
+from tkinter.filedialog import askopenfilename
+import tkinter as tk, os, convert
+
+fileInfo = convert.fileInfo()
+
+def chooseFile():
+    filename = askopenfilename(filetype=[("Playlist file", "*.m3u")])
+    if filename != "":
+        fileInfo.setplaylistPath(filename)
+        readout.configure(state="normal")
+        readout.insert("1.0", "Playlist file set to " + filename + "\n")
+        readout.configure(state="disabled")
+        # chosenFile.configure(state="normal")
+        chosenFileText.set(filename)
+        # chosenFile.configure(state="disabled")
+    elif fileInfo.getplaylistPath != "":
+        pass
+    else:
+        readout.configure(state="normal")
+        readout.insert("1.0", "No file chosen\n")
+        readout.configure(state="disabled")
+    # convert.convert("C:\\Music", "/music", filename, "C:\\Users\\Krauser\\Documents\\test\\playlist-sync\\test")
+    # convert.copy(filename, "C:\\Users\\Krauser\\Documents\\test\\playlist-sync\\test")
+
+def convertAndCopy():
+    oldpath = oldpathInput.get().strip()
+    print(oldpath)
+    newpath = newpathInput.get().strip()
+    print(newpath)
+    pdest = playlistDest.get().strip()
+    print(pdest)
+    mdest = musicDest.get().strip()
+    print(mdest)
+    convert.convert(oldpath, newpath, fileInfo.getplaylistPath(), pdest)
+    convert.copy(fileInfo.getplaylistPath(), mdest)
+
+window = tk.Tk()
+window.title("Welcome to garbage, enjoy your stay")
+
+pathmsg1 = tk.Label(text = "Enter old playlist path format")
+pathmsg2 = tk.Label(text = "Enter new playlist path format")
+pathmsg3 = tk.Label(text = "Enter destination playlist path")
+pathmsg4 = tk.Label(text = "Enter destination music path")
+
+chosenFileText = tk.StringVar()
+
+chosenFile = tk.Entry(
+    window,
+    width = 100,
+    textvariable = chosenFileText,
+    state = "disabled"
+)
+
+oldpathInput = tk.Entry(
+    window,
+    width = 100
+)
+
+newpathInput = tk.Entry(
+    window,
+    width = 100
+)
+
+playlistDest = tk.Entry(
+    window,
+    width = 100
+)
+
+musicDest = tk.Entry(
+    window,
+    width = 100
+)
+
+readout = tk.Text(
+    window,
+    height = 10, 
+    width = 100,
+    bg = "white",
+    fg = "black",
+    state="disabled"
+)
+
+addfile = tk.Button(
+    window,
+    text="Choose .m3u file",
+    width=15,
+    height=1,
+    bg="white",
+    fg="black",
+    command = chooseFile
+)
+
+candc = tk.Button(
+    window,
+    text = "Convert and Copy",
+    width=15,
+    height=1,
+    bg="white",
+    fg="black",
+    command = convertAndCopy
+)
+
+addfile.pack()
+chosenFile.pack(expand=True, fill=tk.X)
+pathmsg1.pack()
+oldpathInput.pack(expand=True, fill=tk.X)
+pathmsg2.pack()
+newpathInput.pack(expand=True, fill=tk.X)
+pathmsg3.pack()
+playlistDest.pack(expand=True, fill=tk.X)
+pathmsg4.pack()
+musicDest.pack(expand=True, fill=tk.X)
+candc.pack()
+readout.pack(expand=True, fill=tk.BOTH)
+
+window.mainloop()
